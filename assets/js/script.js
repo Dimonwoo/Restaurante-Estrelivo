2b735448e62a3dfe091b45ba5da008ed6e2cdcee
DEVitor0 <vitormoreira6940@gmail.com>
2024-02-14 20:11:53 -0300

    criação do script do menu

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menuBtn = document.querySelector('.menu-btn');
+    const menu = document.querySelector('.menu');
+
+    menuBtn.classList.toggle('open');
+    menu.classList.toggle('open');
+}
