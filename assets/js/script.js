4511f83961bc90d8e96a8615445e6239b6ba20e6
DEVitor0 <vitormoreira6940@gmail.com>
2024-02-16 19:35:51 -0300

    criação do efeito carrossel em inicio e  mudança no menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,41 @@ function toggleMenu() {
     menuBtn.classList.toggle('open');
     menu.classList.toggle('open');
 }
+
+const images = ['assets/styles/img/king-burguer.png', 'assets/styles/img/cheese-burguer.png', 'assets/styles/img/american-burguer.png'];
+let currentImageIndex = 0;
+const header = document.querySelector('.carousel-header');
+const dots = document.querySelectorAll('.dot');
+
+function changeHeaderBackground() {
+    header.style.backgroundImage = `url(${images[currentImageIndex]})`;
+}
+
+function updateActiveDot() {
+    dots.forEach((dot, index) => {
+        if (index === currentImageIndex) {
+            dot.classList.add('active');
+        } else {
+            dot.classList.remove('active');
+        }
+    });
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    changeHeaderBackground();
+    updateActiveDot();
+}
+
+dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        currentImageIndex = index;
+        changeHeaderBackground();
+        updateActiveDot();
+    });
+});
+
+changeHeaderBackground();
+updateActiveDot();
+
+setInterval(nextImage, 6000);
